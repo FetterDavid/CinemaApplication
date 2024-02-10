0457237f751893e12792b1d6dd025fbcb7a3c84e
FetterDavid <fetter.david00@gmail.com>
2024-02-10 21:41:07 +0100

    hianyzo kommentek potlasa 2

--- CinemaApplication/CinemaApplication/Server/Controllers/DirectorController.cs
+++ CinemaApplication/CinemaApplication/Server/Controllers/DirectorController.cs
@@ -5,12 +5,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CinemaApplication.Api.Controllers
 {
+    /// <summary>
+    /// Controller for managing director-related operations.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class DirectorController : CinemaControllerBase<Director>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DirectorController"/> class.
+        /// </summary>
+        /// <param name="applicationDbContext">The database context.</param>
         public DirectorController(ApplicationDbContext applicationDbContext) : base(applicationDbContext) { }
-
+        /// <summary>
+        /// Deletes a director and their associated movies by director ID.
+        /// </summary>
+        /// <param name="directorId">The ID of the director to delete.</param>
+        /// <returns>An asynchronous task that returns an IActionResult indicating the result of the operation.</returns>
         [HttpDelete("with-movies/{directorId}")]
         public async Task<IActionResult> GetByDirectorId(int directorId)
         {
